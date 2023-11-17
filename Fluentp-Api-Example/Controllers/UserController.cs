8d4695dfab972b71909a983fb32d351d5d4b0918
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-17 21:58:57 +0500

    save

--- Fluentp-Api-Example/Controllers/UserController.cs
+++ Fluentp-Api-Example/Controllers/UserController.cs
@@ -1,19 +1,38 @@
-﻿using Fluent_Api.Infrastructure.Services.UserServices;
+﻿using Fluent_Api.Infrastructure.Dtos;
+using Fluent_Api.Infrastructure.Services.UserServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fluentp_Api_Example.Controllers
 {
-    [Route("api/[controller]/action")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly IUserService _userservice;
 
         [HttpGet]
-        public  IActionResult GetAllAsync()
+        public   IActionResult GetAllAsync()
         {
-            var result =  _userservice.GetAllAsync();
-            return Ok(result);
+            var result =   _userservice.GetAllAsync();
+            return Ok(result.Result);
+        }
+        [HttpPost]
+        public IActionResult CreateAsync(UserDto user)
+        {
+            var result = _userservice.CreateAsync(user);
+            return Ok(result.Result);
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var result = _userservice.GetByIdAsync(id);
+            return Ok(result.Result);
+        }
+        [HttpPut]
+        public IActionResult UpdateAsync(int id, UserDto user)
+        {
+            var result = _userservice.UpdateAsync(id, user);
+            return Ok(result.Result);
         }
     }
 }
