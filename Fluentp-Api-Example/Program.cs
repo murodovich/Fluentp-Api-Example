817b77030f6c2ada47a301a8ac77996c27da05de
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-17 21:59:13 +0500

    save

--- Fluentp-Api-Example/Program.cs
+++ Fluentp-Api-Example/Program.cs
@@ -1,4 +1,5 @@
 using Faluent_Api.Application;
+using Fluent_Api.Infrastructure.Services.UserServices;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
