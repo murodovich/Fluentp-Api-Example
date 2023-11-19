c2209af26cec1365eb9936aec28621e81856c5c3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-19 22:38:25 +0500

    sav

---
+++ Faluent-Api.Application/Migrations/20231119173716_sarva.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Faluent_Api.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class sarva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookCategoryId",
+                table: "BookCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookCategoryId",
+                table: "BookCategories");
+        }
+    }
+}
