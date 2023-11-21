ab5943f4d7fbbaf7127ed0f6ffeadf2e1e84179b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-21 14:38:37 +0500

    SAVE

---
+++ Faluent-Api.Application/Migrations/20231121093154_saves.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Faluent_Api.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class saves : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
